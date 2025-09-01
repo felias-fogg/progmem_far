f82a9c6983e551e1f7fe7121afdac70c94ea92ab
Bernhard Nebel <nebel@hinterm-ziel.de>
2025-09-01 22:04:35 +0200

    v1.0.1

--- src/progmem_far.h
+++ src/progmem_far.h
@@ -1,6 +1,6 @@
 /* Arduino library: progmem_far.h
  *
- * Version: 1.0.0
+ * Version: 1.0.1
  *
  * Copyright: Bernhard Nebel 2025
  *
@@ -14,16 +14,30 @@
  * The parameter to these macros is always a 32-bit address,
  * which can be determined by pgm_get_far_address. 
  *
+ * v1.0.0
+ * - first working version
+ *
+ * v1.0.1
+ * - renamed the termination function to something hopefully not clashing wit the user sketch
+ * - added weak attribute so that it can be imported into more thna one translation unit
+ * - put an include guard around it
+ *
  */
 
-// We (ab-)use the linker section .fini0, the last section in the .text segment:
+#ifndef PROGMEM_FAR_H
+#define PROGMEM_FAR_H
+
+// We (ab-)use the linker section .fini0, the last section in the .text segment, as the progmem_far segment
 #define PROGMEM_FAR __attribute__((section (".fini0")))
 
 // In order to make sure that after program termination, we end up in an
-// infinite loop, we make use of the user-definable section .fini1.
-void terminate(void) __attribute__((naked, section(".fini1"), used)); 
-void terminate(void)
+// infinite loop, we make use of the user-definable section .fini1, which
+// is placed immediately before .fini0
+void __terminate_program__(void) __attribute__((naked, section(".fini1"), used, weak));
+void __terminate_program__(void)
 {
   while(1);
 }
 
+
+#endif
